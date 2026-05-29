480a07c09ea47a46935be92568dd34b26e622e8f
Dominic Slonka <dslonka06@gmail.com>
2026-05-29 10:53:39 -0400

    yerp

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,18 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
 import { Sidebar } from './components/sidebar/sidebar';
 import { CodeEditor } from './components/code-editor/code-editor';
+import { AnalysisPanel } from './components/analysis-panel/analysis-panel';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Sidebar, CodeEditor],
+  imports: [
+    RouterOutlet,
+    Sidebar,
+    CodeEditor,
+    AnalysisPanel
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
